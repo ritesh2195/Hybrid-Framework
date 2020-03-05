4a61aec733b3fe8811f5d45a64161bc6d8fce306
RITESH RANJAN MISHRA <58725105+ritesh2195@users.noreply.github.com>
2020-03-05 18:04:04 +0530

    Update LoginPage.java

--- src/test/java/IGPPack/Pages/LoginPage.java
+++ src/test/java/IGPPack/Pages/LoginPage.java
@@ -1,12 +1,11 @@
 	package IGPPack.Pages;
 
 	import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.WebElement;
 	import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
+	import org.openqa.selenium.support.PageFactory;
+	import com.relevantcodes.extentreports.ExtentTest;
+	import com.relevantcodes.extentreports.LogStatus;
 	
 	public class LoginPage {
 		
@@ -33,6 +32,10 @@ import com.relevantcodes.extentreports.LogStatus;
 	}
 	
 	public void doLogin(String loginId, String password) {
+		
+	WebDriverWait wait=new WebDriverWait(driver, 20);
+	
+	wait.until(ExpectedConditions.visibilityOf(loginField));
 		
 	loginField.sendKeys(loginId);
 	
